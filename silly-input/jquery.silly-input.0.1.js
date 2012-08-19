4be0a947ec6022646f0e2130f57017d657702f08
Wojtek Turyn <w.turyn@gmail.com>
2012-08-19 21:54:24 +0200

    New plugin draft - sillyInput

---
+++ silly-input/jquery.silly-input.0.1.js
@@ -0,0 +1,32 @@
+/* jQuery sillyInput plugin v 0.1
+ * author: Wojtek Turyn
+ * 
+ * 
+ */
+
+(function($){
+	
+	var I={
+			
+	};
+	
+	var methods={
+		init:function(options) {
+			var settings = $.extend({
+				
+			},options);
+		}	
+	};
+	
+	$.fn.sillyInput = function(method) {
+		 if ( methods[method] ) {
+		      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		    } else if ( typeof method === 'object' || ! method ) {
+		      return methods.init.apply( this, arguments );
+		    } else {
+		    	
+		      $.error( 'Method ' +  method + ' does not exist on jQuery.sillyInput' );
+		    }
+	
+	};
+})(jQuery);
