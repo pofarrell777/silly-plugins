f06cf1d35175b0182a2066327f73e0a720454845
Wojtek Turyn <w.turyn@gmail.com>
2012-09-07 22:19:09 +0200

    popup modification

--- silly-popup/jquery.sillyPopUp.js
+++ silly-popup/jquery.sillyPopUp.js
@@ -14,35 +14,32 @@
 		
 		var data = target.data('sillyPopUp');
 		
-		if (data) {
-			target.show();
-			target.children('.sillyPopUpAnchor').focus();
-			
-		} else {
-			target.addClass('sillyPopUp');
-			target.append('<p class="sillyPopUpText">'+text+'</p>');
-			target.append('<div class="sillyPopUpButton">OK</div>');
-			var a = $('<a class="sillyPopUpAnchor" href="javascript:void(0)"></a>');
-			a.keydown(function() {
-				switch(event.keyCode) {
-				case tvKey.KEY_ENTER:
-				case tvKey.KEY_RETURN:
-					widgetAPI.blockNavigation(event);
-					target.hide();
-					a.blur();
-					callback();
-				break;
-				case tvKey.KEY_EXIT:
-					widgetAPI.blockNavigation(event);
-					widgetAPI.sendExitEvent();
-				break;
-				}
-			});
-			target.append(a);
-			target.data('sillyPopUp',1);
-			target.show();
-			a.focus();
-		}
+		target.children().remove();
+	
+		target.addClass('sillyPopUp');
+		target.append('<p class="sillyPopUpText">'+text+'</p>');
+		target.append('<div class="sillyPopUpButton">OK</div>');
+		var a = $('<a class="sillyPopUpAnchor" href="javascript:void(0)"></a>');
+		a.keydown(function() {
+			switch(event.keyCode) {
+			case tvKey.KEY_ENTER:
+			case tvKey.KEY_RETURN:
+				widgetAPI.blockNavigation(event);
+				target.hide();
+				a.blur();
+				callback();
+			break;
+			case tvKey.KEY_EXIT:
+				widgetAPI.blockNavigation(event);
+				widgetAPI.sendExitEvent();
+			break;
+			}
+		});
+		target.append(a);
+		target.data('sillyPopUp',1);
+		target.show();
+		a.focus();
+	
 
 	};
 	
